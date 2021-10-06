4fd33a4ca96aade593ae44f9227fcbb954867136
vanshitalavange <86543154+vanshitalavange@users.noreply.github.com>
2021-10-06 16:16:14 +0530

    feat : basic functions added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,43 @@ import "./styles.css";
 import calculator from "./images/calculator.svg";
 
 export default function App() {
+  const [number, setNumber] = useState("");
+  const [result, setResult] = useState("");
+
+  function isPrime(number) {
+    var flag = 0;
+    if (number === 1 || number === 0) {
+      flag = 1;
+    } else if (number === 2) {
+      flag = 2;
+    } else if (number > 1) {
+      for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+          flag = 3;
+          break;
+        } else {
+          flag = 2;
+        }
+      }
+    }
+    return flag;
+  }
+  function clickEventHandler() {
+    if (number === "" || number < 0) setResult("Please enter valid input");
+    else {
+      var flag = isPrime(Number(number));
+      if (flag === 1) {
+        setResult(number + " is neither prime nor composite");
+      }
+      if (flag === 2) {
+        setResult(number + " is a prime number");
+      }
+      if (flag === 3) {
+        setResult(number + " is not a prime number");
+      }
+    }
+  }
+
   return (
     <div className="App">
       <h1>Is it a Prime number?</h1>
